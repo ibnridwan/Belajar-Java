3bca875fae7bee7a434eb2fefc14e47b9b5c0abe
ibnridwan <49231613+ibnridwan@users.noreply.github.com>
2019-04-15 17:30:38 +0800

    Delete 01-Tutorial Java.java

--- 01-Tutorial Java.java
+++
@@ -1,9 +0,0 @@
-package com.tutorial;
-
-public class Main {
-
-    public static void main(String[] args) {
-	// write your code here
-        System.out.println("Selamat Datang Muhammad Syahwal");
-    }
-}
